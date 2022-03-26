5aaad90fa90aab84e04e57c0c74be4bdfe8d9583
енгн гшенг <artem_shundeev@mail.ru>
2022-03-26 17:59:26 +0500

    Сделал первый этап

--- ConsoleApplication5/ConsoleApplication5.cpp
+++ ConsoleApplication5/ConsoleApplication5.cpp
@@ -4,18 +4,18 @@
 #include <GL/freeglut.h>
 
 void RenderSceneCB() {
-	glClear(GL_COLOR_BUFFER_BIT);
-	glutSwapBuffers();
+	glClear(GL_COLOR_BUFFER_BIT); //очистка буфера кадра
+	glutSwapBuffers();  //меняем фоновый буфер и буфер кадра местами
 	
 }
 	int main(int argc, char** argv) {
-		glutInit(&argc, argv);
-		glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+		glutInit(&argc, argv); //инициализируем GLUT
+		glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB); //включаем двойную буферизацию и буфер цвета
 		glutInitWindowSize(1024, 768);
-		glutInitWindowPosition(100, 100);
-		glutCreateWindow("Tutorial 01");
+		glutInitWindowPosition(100, 100); //задаем параметры окна и создаем его
+		glutCreateWindow("Tutorial 01"); // заголовок
 		glutDisplayFunc(RenderSceneCB);
-		glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+		glClearColor(0.0f, 0.0f, 0.0f, 0.0f); // задаем цвет окна
 		glutMainLoop();
 		return 0;
 	}
